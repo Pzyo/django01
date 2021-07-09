add028a1ec4bb11c348c4d8f6b1262fbee7dcb6b
欧阳志鹏 <925975498@qq.com>
2021-07-09 16:34:46 +0800

    form组件 1.渲染标签 2.展示提示信息 3.自定义报错信息 4.钩子函数 5.forms其他参数 6.其他类型渲染

--- app05/urls.py
+++ app05/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from app05 import views
 
 urlpatterns = [
-    # form组件前戏
+    # forms组件前戏
     url(r'^ab_form/',views.ab_form),
+    # forms组件渲染标签
+    url(r'^index/',views.index)
 ]
--- app05/views.py
+++ app05/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 
@@ -21,14 +21,130 @@ def ab_form(request):
 
 
 from django import forms
-
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 class MyForm(forms.Form):
     # username字符串类型最小三位, 最大8位
-    username = forms.CharField(min_length=3, max_length=8)
+    username = forms.CharField(min_length=3, max_length=8,label='用户名',initial='pzyo',
+                               error_messages={
+                                   'min_length':'用户名最少3位',
+                                   'max_length':'用户名最大8位',
+                                   'required':'用户名不能为空'
+                               },
+                               widget=forms.widgets.TextInput(attrs={'class':'form-control'})
+                               )
     # password字符串类型最小三位, 最大8位
-    password = forms.CharField(min_length=3, max_length=8)
+    password = forms.CharField(min_length=3, max_length=8,label='密码',
+                                error_messages={
+                                   'min_length':'密码最少3位',
+                                   'max_length':'密码最大8位',
+                                   'required':'密码不能为空'
+                               },
+                               widget=forms.widgets.PasswordInput(attrs={'class':'form-control c1 c2'})
+                               )
+
+    confirm_password = forms.CharField(min_length=3, max_length=8, label='确认密码',
+                               error_messages={
+                                   'min_length': '确认密码最少3位',
+                                   'max_length': '确认密码最大8位',
+                                   'required': '确认密码不能为空'
+                               },
+                                widget=forms.widgets.PasswordInput(attrs={'class':'form-control'})
+                                )
     # email字段必须符合邮箱格式  xxx@xx.com
-    email = forms.EmailField()
+    email = forms.EmailField(label='邮箱',required=False
+                            ,error_messages={
+                                   'invalid':'邮箱格式不正确',
+                                   'required':'邮箱不能为空'
+                            },
+                             widget=forms.widgets.PasswordInput(attrs={'class': 'form-control'})
+                             )
 
+    phone = forms.CharField(
+        validators=[RegexValidator(r'^[0-9]+$', '请输入数字'),
+                    RegexValidator(r'^159[0-9]+$', '数字必须以159开头')],
+    )
 
+    # radio
+    gender = forms.ChoiceField(
+        choices=((1, "男"), (2, "女"), (3, "保密")),
+        label="性别",
+        initial=3,
+        widget=forms.widgets.RadioSelect()
+    )
 
+    # 单选select
+    hobby = forms.ChoiceField(
+        choices=((1, "篮球"), (2, "足球"), (3, "双色球"), ),
+        label="爱好",
+        initial=3,
+        widget=forms.widgets.Select()
+    )
+
+    # 多选select
+    hobby1 = forms.MultipleChoiceField(
+        choices=((1, "篮球"), (2, "足球"), (3, "双色球"), ),
+        label="爱好",
+        initial=[1, 3],
+        widget=forms.widgets.SelectMultiple()
+    )
+
+    # 单选checkbox
+    keep = forms.ChoiceField(
+        label="是否记住密码",
+        initial="checked",
+        widget=forms.widgets.CheckboxInput()
+    )
+
+    # 多选checkbox
+    hobby2 = forms.MultipleChoiceField(
+        choices=((1, "篮球"), (2, "足球"), (3, "双色球"),),
+        label="爱好",
+        initial=[1, 3],
+        widget=forms.widgets.CheckboxSelectMultiple()
+    )
+
+    # 钩子函数
+    # 局部钩子
+    def clean_username(self):
+        # 获取到用户名
+        username = self.cleaned_data.get('username')
+        if '666' in username:
+            # 提示前端展示错误信息
+            # 报错方式1
+            self.add_error('username','光喊666是不行滴~')
+            # 报错方式2 (较为繁琐, 一般不用, 了解)
+            # raise ValidationError('光喊666是不行滴~')
+        # 将钩子函数钩取出来的数据再放回去
+        return username
+
+    # 全局钩子
+    def clean(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+        if not confirm_password == password:
+            self.add_error('confirm_password','两次密码不一致')
+        # 将钩子函数勾出来的数据再放回去
+        return self.cleaned_data
+
+def index(request):
+    # 1. 先产生一个空对象
+    form_obj = MyForm()
+    # 2. 直接将该空对象传递给html页面
+
+    if request.method == 'POST':
+        # 获取用户数据并且校验
+        """
+        1. 数据获取繁琐
+        2. 校验数据需要构造成字典的格式传入才行
+        ps: 但是request.POST可以看成就是一个字典
+        """
+        # 3. 校验数据
+        form_obj = MyForm(request.POST)
+        # 4. 判断数据是否合法
+        if form_obj.is_valid():
+            # 5. 如果合法 操作数据库存储数据
+            return HttpResponse('OK')
+        # 5. 不合法, 有错误
+
+    return render(request,'app05/index.html',locals())
